a1da5f66720af00accf91efdce150667192d9c9c
besaklish <besaklish@gmail.com>
2023-12-10 14:10:01 +0900

    fix: main_comparison outputs to file

--- main_comparison.py
+++ main_comparison.py
@@ -1,3 +1,5 @@
+import os
+from datetime import datetime
 from typing import List
 
 import openai
@@ -17,7 +19,16 @@ def main():
 
     response_message = response.choices[0].message
 
-    with open("output.txt", "w") as f:
+    output_dir = "output_comparison/summary"
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    payload_head = response_message.content[:20].replace("\n", " ")
+    file_name = f"{timestamp}_{payload_head}.md"
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    with open(os.path.join(output_dir, file_name), "w") as f:
         f.write(response_message.content)
 
 
